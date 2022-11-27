8bb9242cab71b1c0c8a26f983703a71b2c43592f
pdongre123 <49409303+pdongre123@users.noreply.github.com>
2022-11-27 18:55:14 +0530

    adding new projects on repository

---
+++ class.js
@@ -0,0 +1,9 @@
+// var myName="pooja dongre";
+// var _myName="PD";
+// var_1my__Name="Dongre";
+// // var 1myName="thapa";
+// var $myName="technical";
+// console.log($myName);
+
+console.log(10+"20");
+console.log(9-'5');

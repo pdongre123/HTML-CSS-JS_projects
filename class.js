84dd419938d5e549a0b9143f26465893b11e60e0
pdongre123 <49409303+pdongre123@users.noreply.github.com>
2023-02-18 19:50:38 +0530

    not working.

--- class.js
+++
@@ -1,9 +0,0 @@
-// var myName="pooja dongre";
-// var _myName="PD";
-// var_1my__Name="Dongre";
-// // var 1myName="thapa";
-// var $myName="technical";
-// console.log($myName);
-
-console.log(10+"20");
-console.log(9-'5');

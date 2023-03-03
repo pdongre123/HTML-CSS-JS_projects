83fd0aa334e965ccc48300c80180cb6ff6b1240b
pdongre123 <49409303+pdongre123@users.noreply.github.com>
2023-03-03 19:07:17 +0530

    js design complete

---
+++ bootcamp/app.js
@@ -0,0 +1,8 @@
+var typed=new Typed(".multiple-text",{
+    strings:["FrontEnd Developer","Youtuber","Digital Creator"],
+    typeSpeed:100,
+    typeSpeed:100,
+    backDelay:1000,
+    loop:true
+
+})
